a7517f02b158bf70e374b0618dd837dc7776c564
Kankitsu3194 <bana3194@gmail.com>
2021-03-08 08:23:55 +0900

    Project Created

---
+++ Assets/Scripts/CrachDetectorScript.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class CrachDetectorScript : MonoBehaviour{
+	private GameObject mainSystem;
+	private MainSystemScript mainSystemScript;
+	void Start(){
+		mainSystem = GameObject.FindGameObjectWithTag("MainSystem");
+		mainSystemScript = mainSystem.GetComponent<MainSystemScript>();
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("Ball")) {
+			Destroy(other.gameObject);
+			mainSystemScript.Crash();
+		}
+	}
+}
---
+++ Assets/Scripts/MainSystemScript.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class MainSystemScript : MonoBehaviour {
+	private int life = 3;
+	private long score = 0;
+	private GameObject spowner;
+
+	public GameObject ballPrefab;
+	void Start() {
+		spowner = GameObject.Find("BallSpowner");
+		BallSpown();
+	}
+
+	public void Crash() {
+		life -= 1;
+		if (life > 0) {
+			BallSpown();
+		} else {
+			Gameover();
+		}
+	}
+
+	public void BallSpown() {
+		Instantiate(ballPrefab, spowner.transform.position, Quaternion.identity);
+	}
+
+	public void Gameover() {
+		Debug.Log("Gameover!! Score:" + score);
+	}
+	public void AddScore(int addPoint) {
+		Debug.Log("Add Score:" + addPoint);
+		score += addPoint;
+	}
+	public void AddLife(int addNum) {
+		life += addNum;
+	}
+}
