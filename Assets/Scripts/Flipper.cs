8147acecdb9f03597cd3f9b0e8f7017b6a53b7c5
NakanoK <k.nkn.amtk@outlook.jp>
2021-03-14 16:17:07 +0900

    Plunger finish

---
+++ Assets/Scripts/Flipper.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flipper : MonoBehaviour
+{
+    public float restPosition = 0.0f;
+    public float pressedPosition = 45f;
+    public float hitStrenght = 2000f;
+    public float flipperDamper = 150f;
+    HingeJoint hinge;
+    public string inputName;
+
+    void Start()
+    {
+        hinge = GetComponent<HingeJoint>();
+        hinge.useSpring = true;
+    }
+
+    
+    void Update()
+    {
+        JointSpring spring = new JointSpring();
+        spring.spring = hitStrenght;
+        spring.damper = flipperDamper;
+
+        if(Input.GetAxis(inputName) == 1)
+        {
+
+        }
+        else
+        {
+            spring.targetPosition = restPosition;
+        }
+        hinge.spring = spring;
+        hinge.useLimits = true;
+    }
+}
--- Assets/Scripts/PlungerSystem.cs
+++ Assets/Scripts/PlungerSystem.cs
@@ -4,37 +4,45 @@ using UnityEngine;
 
 public class PlungerSystem : MonoBehaviour
 {
-    GameObject Plunger;
-    float power = 0.1f;
-    float maxPower = 30.0f;
+ 
     public Rigidbody rd;
     Vector3 sp;
     float speed = 0.0f;
+    float time = 0.0f;
     public float maxSpeed = 10.0f;
+    public float power = 10.0f;
+    SpringJoint springJoint;
+
     // Start is called before the first frame update
     void Start()
     {
         rd = GetComponent<Rigidbody>();
         sp = this.transform.position;
+        springJoint = GetComponent<SpringJoint>();
+        //var SpringJoint = GetComponent<SpringJoint>;
+        
     }
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(springJoint.currentForce);
+        //var SpringJoint = GetComponent<SpringJoint>;
         if (Input.GetKey(KeyCode.Space))//押してる時
         {
-            if (this.power < maxPower)
+            //SpringJoint.enabled = false;
+            if (time < 10.0f)
             {
-                this.transform.position += new Vector3(0, -0.006f, -0.008f);//位置
-
-                this.power *= 1.02f;//力
+                this.transform.position += new Vector3(0, -0.2f, -0.34f);//位置
+                time += 0.1f;
             }
-            else if (this.power >= maxPower) this.power = maxPower;
         }
         if (Input.GetKeyUp(KeyCode.Space))//離したとき
         {
-            GetComponent<Rigidbody>().MovePosition(sp);
-            //rd.velocity = new Vector3(0, 1.0f, 1.7f)*10.0f;
+            //GetComponent<Rigidbody>().MovePosition(sp);
+            //Vector3 force = new Vector3(0, 1.0f * power, 1.7f * power);
+            //rd.AddForce(force, ForceMode.Impulse);
+            //SpringJoint.enabled = true;
+            this.time = 0.0f;
         }
     }
-
 }
